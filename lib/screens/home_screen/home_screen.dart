1dd66ce76b51515823977d2d1693ac10139fb0b6
amanc1248 <amanc1248@gmail.com>
2021-05-31 22:24:54 +0545

    add home page

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,21 +1,350 @@
-import 'package:flutter/material.dart';
-import 'package:gudri/screens/home_screen/home_screen_mobile/home_screen_mobile_lanscape.dart';
-import 'package:gudri/screens/home_screen/home_screen_mobile/home_screen_mobile_potrait.dart';
-import 'package:gudri/ui_config/orientation_layout.dart';
-import 'package:gudri/ui_config/screen_type_layout.dart';
+import 'dart:ui';
 
-class HomeScreen extends StatelessWidget {
-  static const id = "home_screen";
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:gudri/screens/home_screen/localWidgets/LocalWidgets.dart';
+
+class AppAndDrawer extends StatefulWidget {
+  static const String id = 'AppAndDrawer';
+
+  @override
+  _AppAndDrawerState createState() => _AppAndDrawerState();
+}
+
+class _AppAndDrawerState extends State<AppAndDrawer> {
   @override
   Widget build(BuildContext context) {
-    return ScreenTypeLayout(
-      mobile: OrientationLayout(
-        portrait: HomeScreenMobilePortrait(),
-        landscape: HomeScreenMobileLandscape(),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Container(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            // For the locaiton.
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Delivery Location",
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+                  ),
+                  Text(
+                    "Duhabi-ward no. 5",
+                    style: TextStyle(fontSize: 16),
+                  )
+                ],
+              ),
+            ),
+            // For the cart
+            Icon(
+              FontAwesomeIcons.shoppingCart,
+              size: 20,
+            )
+          ],
+        )),
+        backgroundColor: Color(0xff00008b),
       ),
-      tablet: OrientationLayout(
-        portrait: HomeScreenMobilePortrait(),
-        landscape: HomeScreenMobileLandscape(),
+      body: HomeScreen(),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration:
+                  BoxDecoration(color: Color.fromRGBO(255, 254, 138, 0.8)),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image(
+                    image: AssetImage("assets/images/logo.png"),
+                    height: 100,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Goodri",
+                        style: TextStyle(fontSize: 30, color: Colors.blue),
+                      ),
+                      Text("The Local Online Store")
+                    ],
+                  )
+                ],
+              ),
+            ),
+            ListTile(leading: Text("image"), title: Text("Namaskar")),
+            ListTile(
+              leading: Icon(Icons.location_on),
+              title: Text('Ward-5 Duhabi'),
+              trailing: Icon(Icons.edit),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Login'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.location_on),
+              title: Text('Mero Orders'),
+              trailing: Container(
+                width: 25,
+                height: 25,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(50)),
+                    color: Colors.red),
+                child: Center(
+                    child: Text("1", style: TextStyle(color: Colors.white))),
+              ),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(FontAwesomeIcons.shoppingCart),
+              title: Text('Mero Cart'),
+              trailing: Container(
+                width: 25,
+                height: 25,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(50)),
+                    color: Colors.red),
+                child: Center(
+                    child: Text("1", style: TextStyle(color: Colors.white))),
+              ),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.notifications),
+              title: Text('Mero Notifications'),
+              trailing: Container(
+                width: 25,
+                height: 25,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(50)),
+                    color: Colors.red),
+                child: Center(
+                    child: Text("1", style: TextStyle(color: Colors.white))),
+              ),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+
+            //  Others division.
+            ListTile(
+              title: Text('Others'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Customer Helpline'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.star),
+              title: Text('Rate us'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('About Us'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('About this release'),
+              trailing: Text("V.1.0", style: TextStyle(color: Colors.grey)),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(color: Color(0xff00008b)),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 2),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        height: 34,
+                        child: Center(child: Text("Categories")),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.all(Radius.circular(4))),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 4,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        height: 34,
+                        child: Row(
+                          children: [
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Icon(
+                              Icons.search,
+                              color: Color(0xffEDEDED),
+                            ),
+                            SizedBox(
+                              width: 6,
+                            ),
+                            Text(
+                              "Search for Products",
+                              style: TextStyle(
+                                  color: Color(0xff777777), letterSpacing: 1),
+                            )
+                          ],
+                        ),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(Radius.circular(4)),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+
+          // We are accepting widget
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+            child: Row(
+              children: [
+                Icon(Icons.lock_clock),
+                SizedBox(
+                  width: 16,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("We are accepting orders now!",
+                        style: TextStyle(fontWeight: FontWeight.w500)),
+                    Text("Earliest delivery by tomorrow 9 AM",
+                        style: TextStyle(fontWeight: FontWeight.w500))
+                  ],
+                )
+              ],
+            ),
+          ),
+
+          // for best savers 👇👇
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SectionTitle(
+                  title: "Best Savers",
+                ),
+                SectionEndTitle(endTitle: "See All")
+              ],
+            ),
+          ),
+          Container(
+            height: 400,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [Product(), Product(), Product(), Product()],
+            ),
+          ),
+
+          //for category title 👇👇👇
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SectionTitle(
+                  title: "Shop by Category",
+                ),
+              ],
+            ),
+          ),
+
+          // Category Card 👇👇
+          Container(child: CategoryCard())
+        ],
       ),
     );
   }
---
+++ lib/screens/home_screen/localWidgets/LocalWidgets.dart
@@ -0,0 +1,208 @@
+import 'package:flutter/material.dart';
+import 'package:gudri/models/categoryModel.dart';
+
+// section title👇👇
+class SectionTitle extends StatefulWidget {
+  final String title;
+  // final String trailTitle;
+  SectionTitle({required this.title});
+  @override
+  _SectionTitleState createState() => _SectionTitleState();
+}
+
+class _SectionTitleState extends State<SectionTitle> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          widget.title,
+          style: TextStyle(
+              fontWeight: FontWeight.w900, fontSize: 25, fontFamily: "Cabin"),
+        ),
+        Container(
+          decoration: BoxDecoration(
+              border: Border.all(color: Color(0xffDEDEDE)),
+              borderRadius: BorderRadius.all(Radius.circular(4))),
+        )
+      ],
+    );
+  }
+}
+
+// section end title 👇👇
+class SectionEndTitle extends StatefulWidget {
+  final String endTitle;
+  const SectionEndTitle({required this.endTitle});
+  @override
+  _SectionEndTitleState createState() => _SectionEndTitleState();
+}
+
+class _SectionEndTitleState extends State<SectionEndTitle> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(color: Color(0xffDEDEDE)),
+          borderRadius: BorderRadius.all(Radius.circular(4))),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+        child: Text(
+          widget.endTitle,
+          style: TextStyle(
+              fontWeight: FontWeight.w900,
+              fontSize: 20,
+              fontFamily: "Cabin",
+              color: Color(0xffDC6311)),
+        ),
+      ),
+    );
+  }
+}
+
+// product 👇👇
+class Product extends StatefulWidget {
+  @override
+  _ProductState createState() => _ProductState();
+}
+
+class _ProductState extends State<Product> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 190,
+      margin: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+      decoration: BoxDecoration(
+        border: Border.all(color: Color(0xffDEDEDE)),
+        borderRadius: BorderRadius.all(Radius.circular(8)),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image(image: AssetImage("assets/images/logo.png")),
+          Text(
+            "₹161",
+            style: TextStyle(
+              fontSize: 25,
+              fontFamily: "Cabin",
+              fontWeight: FontWeight.w900,
+            ),
+          ),
+          Text(
+            "Soyabean Soyabean Soyabean Soyabean Soyabean",
+            style: TextStyle(
+                fontSize: 19,
+                fontFamily: "Cabin",
+                fontWeight: FontWeight.w600,
+                color: Color(0xffA9A9A9)),
+          ),
+          Text(
+            "1 L",
+            style: TextStyle(
+                fontSize: 19,
+                fontFamily: "Cabin",
+                fontWeight: FontWeight.w600,
+                color: Color(0xffA9A9A9)),
+          ),
+          Container(
+            // height: 40,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20))),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                    height: 30,
+                    decoration: BoxDecoration(color: Color(0xffE96125)),
+                    child: Center(
+                      child: Text(
+                        "ADD",
+                        style: TextStyle(
+                            fontSize: 19,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: "Cabin"),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    height: 30,
+                    decoration: BoxDecoration(
+                      color: Color(0xffC0511D),
+                    ),
+                    child: Icon(
+                      Icons.add,
+                      size: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+// category 👇👇
+final List<CategoryModel> categoryList = [
+  CategoryModel(image: "assets/images/logo.png", title: "Grocery & Staples"),
+  CategoryModel(image: "assets/images/logo.png", title: "Kitchening & Dining"),
+  CategoryModel(image: "assets/images/logo.png", title: "Household Items"),
+  CategoryModel(image: "assets/images/logo.png", title: "Personal Care"),
+  CategoryModel(image: "assets/images/logo.png", title: "Fruits & Vegetables"),
+  CategoryModel(image: "assets/images/logo.png", title: "Bevarages"),
+];
+
+class CategoryCard extends StatefulWidget {
+  @override
+  _CategoryCardState createState() => _CategoryCardState();
+}
+
+class _CategoryCardState extends State<CategoryCard> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      itemCount: categoryList.length,
+      itemBuilder: (context, index) {
+        return Container(
+          width: 120,
+          decoration: BoxDecoration(
+              border: Border.all(
+                color: Color(0xffFD5606),
+                width: 2.5,
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(8))),
+          child: Column(
+            children: [
+              Image(image: AssetImage('${categoryList[index].image}')),
+              Container(
+                  height: 40,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Color(0xffFD5606),
+                    // borderRadius: BorderRadius.all(Radius.circular(8)),
+                  ),
+                  child: Text('${categoryList[index].title}'.toUpperCase(),
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w800,
+                          color: Colors.white)))
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
